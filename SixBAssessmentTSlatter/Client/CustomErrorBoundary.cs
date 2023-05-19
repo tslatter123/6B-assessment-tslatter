b5a61037159e9b3d218e27a6fec0424245eb6c22
Tom Slatter <tjs98_@outlook.com>
2023-05-19 19:54:57 +0100

    Added custom error boundary for later use

---
+++ SixBAssessmentTSlatter/Client/CustomErrorBoundary.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace SixBAssessmentTSlatter.Client
+{
+    public class CustomErrorBoundary : ErrorBoundary
+    {
+        //protected override Task OnErrorAsync(Exception exception)
+        //{
+        //    return Task.CompletedTask;
+        //}
+    }
+}
