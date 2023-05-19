8cde101391ce49b42f1948ab83f756a585c9b05f
Tom Slatter <tjs98_@outlook.com>
2023-05-19 20:04:33 +0100

    Added controller for adding a booking

---
+++ SixBAssessmentTSlatter/Server/Controllers/AddBookingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SixBAssessmentTSlatter.Server.Data;
+using SixBAssessmentTSlatter.Shared.Models;
+
+namespace SixBAssessmentTSlatter.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddBookingController : ControllerBase
+    {
+        ApplicationDbContext _context;
+
+        public AddBookingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddBooking(Booking booking)
+        {
+            try
+            {
+                _context.Add(booking);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
