cc3ebb2c192807ae4ec6784d9cef118668a23416
Tom Slatter <tjs98_@outlook.com>
2023-05-19 19:07:50 +0100

    19:07 - Assessment started

--- SixBAssessmentTSlatter/Client/Program.cs
+++ SixBAssessmentTSlatter/Client/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SixBAssessmentTSlatter.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
