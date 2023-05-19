8d450b66cb15f7075635e1e780390fa4e6075b8c
Tom Slatter <tjs98_@outlook.com>
2023-05-19 19:20:43 +0100

    Added client factory to bookign service

--- SixBAssessmentTSlatter/Client/Services/BookingService.cs
+++ SixBAssessmentTSlatter/Client/Services/BookingService.cs
@@ -4,6 +4,11 @@ namespace SixBAssessmentTSlatter.Client.Services
 {
     public class BookingService : IBookingService
     {
-        
+        private readonly IHttpClientFactory _clientFactory;
+
+        public BookingService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
     }
 }
