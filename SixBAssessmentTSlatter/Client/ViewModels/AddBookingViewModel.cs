0aa0ff3004b01dad81a5c80514e7453b70e5ea58
Tom Slatter <tjs98_@outlook.com>
2023-05-19 19:26:37 +0100

    Added booking view models

---
+++ SixBAssessmentTSlatter/Client/ViewModels/AddBookingViewModel.cs
@@ -0,0 +1,26 @@
+﻿using SixBAssessmentTSlatter.Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SixBAssessmentTSlatter.Client.ViewModels
+{
+    public class AddBookingViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public DayFlexibilityEnum Flexibility { get; set; }
+
+        [Required]
+        public VehicleSizeEnum VehicleSize { get; set; }
+
+        [Required]
+        public string ContacyNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ SixBAssessmentTSlatter/Client/ViewModels/BookingViewModel.cs
@@ -0,0 +1,32 @@
+﻿using SixBAssessmentTSlatter.Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SixBAssessmentTSlatter.Client.ViewModels
+{
+    public class BookingViewModel
+    {
+        [Required]
+        public string ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime? Date { get; set; }
+
+        [Required]
+        public DayFlexibilityEnum DayFlexibility { get; set; }
+
+        [Required]
+        public VehicleSizeEnum VehicleSize { get; set; }
+
+        [Required]
+        public string ContactNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public bool IsApproved { get; set; }
+    }
+}
