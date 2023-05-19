b458e51b200824dea506be99aa8ef47fccf8cc86
Tom Slatter <tjs98_@outlook.com>
2023-05-19 19:33:19 +0100

    Added booking mapper as service to program

--- SixBAssessmentTSlatter/Client/Program.cs
+++ SixBAssessmentTSlatter/Client/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SixBAssessmentTSlatter.Client;
 using SixBAssessmentTSlatter.Client.Interfaces;
+using SixBAssessmentTSlatter.Client.Interfaces.Mappers;
+using SixBAssessmentTSlatter.Client.Mappers;
 using SixBAssessmentTSlatter.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -14,6 +16,8 @@ builder.Services.AddHttpClient("SixBAssessmentTSlatter.ServerAPI", client => cli
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 builder.Services.AddSingleton<IBookingService, BookingService>();
+
+builder.Services.AddSingleton<IBookingMapper, BookingMapper>();
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("SixBAssessmentTSlatter.ServerAPI"));
