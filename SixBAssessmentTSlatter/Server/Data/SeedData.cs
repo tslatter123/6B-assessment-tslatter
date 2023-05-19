310e1da959fc371f0f9713b918d92e29402cdc5b
Tom Slatter <tjs98_@outlook.com>
2023-05-19 21:15:05 +0100

    Added seed to add users by default

---
+++ SixBAssessmentTSlatter/Server/Data/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using SixBAssessmentTSlatter.Server.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorPracticeApp.Server.Data
+{
+    public class SeedData
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public SeedData(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task RunSeed()
+        {
+            if (await _userManager.FindByEmailAsync("test@test.tst") == null)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = "user@6b.tst",
+                    Email = "user@6b.tst",
+                    EmailConfirmed = true
+                };
+                var result = await _userManager.CreateAsync(user, "Testing123!");
+            }
+        }
+    }
+}
